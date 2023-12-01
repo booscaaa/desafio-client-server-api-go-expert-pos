140d0ae8bf1c4dd7f9fe3240207f92d7eabfd4e8
Vinícius Boscardin <boscardinvinicius@gmail.com>
2023-12-01 11:14:10 -0300

    :sparkles: feat(rest.go): add router handlers

---
+++ style-1/adapter/http/rest/rest.go
@@ -0,0 +1,21 @@
+package rest
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/desafio-client-server-api-go-expert-pos/style-1/di"
+	"gorm.io/gorm"
+)
+
+func InitializeRest(serverPort string, database *gorm.DB, client *http.Client) {
+	quoteHTTPService := di.ConfigQuoteDIService(database, client)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/cotacao", quoteHTTPService.Get)
+
+	log.Println(fmt.Sprintf("Rodando o servidor na porta: %s", serverPort))
+	http.ListenAndServe(fmt.Sprintf(":%s", serverPort), mux)
+}
